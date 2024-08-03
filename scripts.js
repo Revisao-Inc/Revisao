0b68307aa9214557295fb6eeef776417fb7d3cf5
User <164103302+Arthrxfpz@users.noreply.github.com>
2024-08-03 00:31:09 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -85,7 +85,7 @@ function closeMenu(event) {
             // Transição para ocultar todas as abas
             tabContents.forEach(content => {
                 content.classList.add('hidden');
-                setTimeout(() => content.classList.remove('active'), 300);
+                setTimeout(() => content.classList.remove('active'), 500);
             });
 
             // Mostra a aba selecionada com transição
@@ -93,7 +93,7 @@ function closeMenu(event) {
             setTimeout(() => {
                 tabContent.classList.add('active');
                 tabContent.classList.remove('hidden');
-            }, 300);
+            }, 500);
 
             // Adiciona estado ao histórico
             const tabId = this.getAttribute('href').substring(1);
